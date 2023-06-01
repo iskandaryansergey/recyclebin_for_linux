255d9348348053e613d83d3c6978d47475959456
Sergey Iskandaryan <seregaiskandaryan14@gmail.com>
2023-06-01 08:59:32 +0300

    Fix trashing_file function

--- recycle.sh
+++ recycle.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
+#!/bin/zsh
+shopt -s nullglob
 # This script will implement recycle bin for the Unix based systems
 # Due to Unix based repos do not have recycle bin 
-
-
 recyclebin=$HOME/recyclebin
 restore_file=$HOME/.restore.info
 
@@ -18,6 +16,69 @@ if [[ ! -a $restore_file ]]
     mkdir $restore_file
 fi
 
+function recycle () {
+  for file in $@
+    do      
+      if [[ -a  $file ]]
+        then
+          full_path=$(readlink -f $file)
+          filename=$(readlink -f $file  | awk -F /  '{print $NF}')
+          file_type=$(file $full_path | cut -d : -f2 | tr -s ' ' | tr -d ' ' )
+          if [[ $interactive  ]]
+            then
+              echo "Do you want to remove this $file file [Y/n] ? "
+         
+              #calling interactive prompt
+              inter_prompt
+          fi
+          
+          move_to_trash
+      
+        else
+          echo -e "\n File do not exist \n"
+      fi
+    done    
+}
+
+
+function trashing_file () { 
+  #move file or dir to the  the RecycleBin
+   if [[ -d $file ]]
+    then
+      if [[  $recursive_flag -ne 0 ]]
+        then
+          # cheking if the dir is empty
+          if  [[  !  -z $(find $file -maxdepth 0 -empty)  ]]
+            then
+              break
+              # should be return if folder is empty
+          fi
+          mkdir -p ${recyclebin}/${trashed_dir_name}
+          local i
+          echo $i
+          for i in $file/*
+            do
+              echo "cikl"
+              file="$i"
+              trashing_file
+              recursive_flag=0
+              recycle $i
+            done
+        else
+          echo "If not recursive creating same dir in the recyclebin" 
+          #mkdir -p ${recyclebin}/${trashed_dir_name}
+      fi 
+         echo $file
+        echo "directorian pti sikitir arvi ete recursive fracela vrov kam chi fracel"
+        
+        # rm -rf $file  
+    else
+      echo "if not dir move file to trash"
+      #mv $file ${recyclebin}/${trashed_file_name}
+   fi
+
+}
+
 function verbose () {
   #checking if verbose flag is persist and print msg
   if [[ $verbose_flag ]]
@@ -29,7 +90,7 @@ function verbose () {
 function move_to_trash () {
   if [[ $remove -eq 2 ]]
     then
-      #exiting due to file will not be removed
+      #exiting due to file will not be removed  here sholud be break due to remove is set to none 
       echo "exiting"
     else
       #removing file
@@ -40,16 +101,32 @@ function move_to_trash () {
 
       if [[ -f $file  ]]
         then
+          inode=$(ls -i $file | cut -d ' ' -f 1)
+         # unset $trashed_file_name
           #Adding record to the 
-          if [[ !$(grep -q ${filename}_ $restore_file) ]]
+          if [[ ! $(grep -q ${filename}_ $restore_file) ]]
             then
-              echo "${filename}_${inode}:${full_path}_`date  +"%H%M%d%m%y"`" >> $restore_file 
+              trashed_file_name=${filename}_${inode}:${full_path}_$(date  +"%H%M%d%m%y") 
+              echo $trashed_file_name >> $restore_file 
             else
-              echo "${filename}_${inode}:${full_path}" >> $restore_file 
+              trashed_file_name=${filename}_${inode}:${full_path}
+              echo $trashed_file_name >> $restore_file 
           fi
-        else
-          echo "dir"
+        elif [[ -d $file ]]
+          then
+            #unset trashed_dir_name
+                if [[ ! $(grep -q ${filename}_dir $restore_file) ]]
+                  then
+                    trashed_dir_name=${filename}_dir_$(date  +"%H%M%d%m%y")
+                    echo $trashed_dir_name >> $restore_file 
+                  else
+                    trashed_dir_name=${filename}_dir
+                    echo $trashed_dir_name >> $restore_file 
+                fi
       fi
+      #calling trashing_file metod
+      trashing_file
+
   fi
 }
 
@@ -76,28 +153,6 @@ function inter_prompt () {
     done  
 }
 
-function recycle () {
-  for file in $@
-    do      
-      if [[ -a  $file ]]
-        then
-          full_path=$(readlink -f $file)
-          filename=$(readlink -f $file  | awk -F /  '{print $NF}')
-          file_type=$(file $full_path | cut -d : -f2 |  tr [:lower:] [:upper:])
-          inode=$(ls -i $file | cut -d ' ' -f 1)
-          if [[ $interactive  ]]
-            then
-              echo "Do you want to remove this $file file [Y/n] ? "
-              inter_prompt
-          fi
-          
-          move_to_trash
-      
-        else
-          echo -e "\n File do not exist \n"
-      fi
-    done    
-}
 
 function main () {
 	if [[ $# < 1  ]]
@@ -108,28 +163,33 @@ function main () {
 	
 		
   for opt in $@
-	do	
-		case $opt
-		in	
-			-i)
-				echo "Choosen interactive"
-				shift
-				interactive=1
-				;;
-			-v)
-				echo "Enter verbose mode"
-				verbose_flag=1
-				shift	
-				;;
-      *)
-        #Exiting from loop 
-        break
-        ;;
-		esac
-	done
-  
-  recycle $@
-			
+   do	
+     case $opt
+       in	
+         -i)
+           echo "Choosen interactive"
+           shift
+           interactive=1
+           ;;
+         -v)
+           echo "Enter verbose mode"
+           verbose_flag=1
+           shift	
+           ;;
+         -r)
+           echo "You have choose recursive metod for the Directory"
+           recursive_flag=1
+           shift
+           ;;
+         *)
+           #Exiting from loop 
+           break
+           ;;
+     esac
+  done
+
+recycle $@
+		
 }
 
 
